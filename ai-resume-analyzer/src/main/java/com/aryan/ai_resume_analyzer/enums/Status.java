bbabc611d615426398ea88849ee275c37185ec5b
Aryan raj <aryanbanthu@gmail.com>
2026-04-20 09:19:44 +0530

    Defined 4 enums to maintain consistent state knowledge b/w diff. systems.

--- ai-resume-analyzer/src/main/java/com/aryan/ai_resume_analyzer/enums/Status.java
+++ ai-resume-analyzer/src/main/java/com/aryan/ai_resume_analyzer/enums/Status.java
@@ -1,4 +1,8 @@
 package com.aryan.ai_resume_analyzer.enums;
 
 public enum Status {
+    UPLOADED,   //Recieved by Backend and Stored locally
+    PROCESSING, //currently being parsed or sent to AI
+    COMPLETED,  //AI analysis finished and results are saved
+    FAILED,     //Error in analysis
 }
