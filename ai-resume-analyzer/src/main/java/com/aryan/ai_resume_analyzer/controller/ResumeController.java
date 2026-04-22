324f01accdd68226a12b24aafedd200346f33222
Aryan raj <aryanbanthu@gmail.com>
2026-04-22 14:39:29 +0530

    imports for java.nio

--- ai-resume-analyzer/src/main/java/com/aryan/ai_resume_analyzer/controller/ResumeController.java
+++ ai-resume-analyzer/src/main/java/com/aryan/ai_resume_analyzer/controller/ResumeController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.core.io.*;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RequiredArgsConstructor
