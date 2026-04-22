77fa902c67da4c9645507b99c5d275fbaeaa53d4
Aryan raj <aryanbanthu@gmail.com>
2026-04-22 14:48:27 +0530

    Added User Entity for one to many mapping.

---
+++ ai-resume-analyzer/src/main/java/com/aryan/ai_resume_analyzer/model/User.java
@@ -0,0 +1,23 @@
+package com.aryan.ai_resume_analyzer.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true,nullable = false)
+    private String username;
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Resume> resumes;
+}
